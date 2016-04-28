ddee76b01043ae97350e5ba3d2df961995c41e81
hayleefay <hayleeham@gmail.com>
2016-04-28 15:02:03 -0600

    Fixing indentation

--- main.rb
+++ main.rb
@@ -34,21 +34,21 @@ puts "\tDone!"
 # Grab the interesting information from each item returned
 puts "Parsing response..."
 r.body.each do |i|
-	raw_message = JSON.parse(i["_raw"])
+  raw_message = JSON.parse(i["_raw"])
 
-	fields = raw_message["fields"] unless raw_message.nil?
-	log_level = fields["level"] unless fields.nil?
+  fields = raw_message["fields"] unless raw_message.nil?
+  log_level = fields["level"] unless fields.nil?
 
-	unless log_level.nil?
-		if log_level.downcase == "info" && fields["method"].downcase == "logbouncedemail"
-			message_json = '{' + raw_message["message"].split('{')[1]
-			message_json = message_json.split('}')[0] + '}'
-			message_json = JSON.parse(message_json)
+  unless log_level.nil?
+    if log_level.downcase == "info" && fields["method"].downcase == "logbouncedemail"
+      message_json = '{' + raw_message["message"].split('{')[1]
+      message_json = message_json.split('}')[0] + '}'
+      message_json = JSON.parse(message_json)
 
-			puts "\n\n"
-			puts message_json["Diagnostic-Code"].gsub('x-postfix; ', '')
-			puts "\n\n"
-		end
-	end
+      puts "\n\n"
+      puts message_json["Diagnostic-Code"].gsub('x-postfix; ', '')
+      puts "\n\n"
+    end
+  end
 end
 puts "\tDone!"
